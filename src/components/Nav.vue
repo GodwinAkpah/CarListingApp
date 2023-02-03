13c61ca2e701f029aea34bd9808192ec94700766
GodwinAkpah <gakpah96@gmail.com>
2023-02-03 04:16:13 +0100

    first commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+<template>
+    <div>
+        <RouterLink active-class="active" to="/" >home</RouterLink>
+        <RouterLink active-class="active" to="/about">about</RouterLink>
+    </div>
+</template>
+<style scoped>
+
+.active{
+    font-weight: 900;
+    color: rgb(153, 104,  14);
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import {createRouter, createWebHistory} from "vue-router"
+import Homeview from "../views/HomeView.vue"
+import AboutView from "../views/AboutView.vue"
+import CarView from "../views/CarView.vue"
+import ContactView from "../views/ContactView.vue"
+import NotFoundView from "../views/404view.vue"
+
+const router = createRouter({
+    history : createWebHistory(import.meta.env.BASE_url),
+    routes:[
+            {
+                path:"/",
+                name:"home",
+                component:Homeview
+
+            },
+            {
+                path: "/home",
+                redirect: "/"
+            },
+            {
+                path:"/about",
+                name :"about",
+                component:AboutView
+            },
+            {
+                path:"/cars/:id",
+                name: "car",
+                component:CarView,
+                children:
+                [
+                    {
+                        path: "contact",
+                        component: ContactView   
+     
+                     }
+                ]
+
+
+            },
+            {
+                path: "/:catchall(.*)*",
+                name:"Not Found",
+                component: NotFoundView
+            }
+]
+
+
+})
+export default router
